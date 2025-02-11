70fa0ef3fa5b2d5b7e2715706f689cde25bd3e36
Rama Pramudhita Bhaskara <2241720128@student.polinema.ac.id>
2025-02-11 10:49:08 +0700

    fix : detail menu api logic

--- lib/features/detail_menu/controllers/detail_menu_controller.dart
+++ lib/features/detail_menu/controllers/detail_menu_controller.dart
@@ -1,7 +1,5 @@
 import 'package:get/get.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:venturo_core/features/detail_menu/repositories/detail_menu_repository.dart';
 
 class DetailMenuController extends GetxController {
   static DetailMenuController get to => Get.find();
@@ -13,30 +11,18 @@ class DetailMenuController extends GetxController {
   final RxList<String> selectedToppings = <String>[].obs;
   final RxString catatan = ''.obs;
   final RxInt totalPrice = 0.obs;
-  final String _token = dotenv.env['API_TOKEN'] ?? '';
+
+  final DetailMenuRepository repository = DetailMenuRepository();
 
   Future<void> fetchMenuDetail(int id) async {
-    var headers = {
-      'token': _token,
-    };
-    var request = http.Request(
-        'GET', Uri.parse('https://trainee.landa.id/javacode/menu/detail/$id'));
-
-    request.headers.addAll(headers);
-
-    http.StreamedResponse response = await request.send();
-
-    if (response.statusCode == 200) {
-      var responseData = await response.stream.bytesToString();
-      print('API Response Data: $responseData'); // Log API response
-      var data = json.decode(responseData)['data'];
+    try {
+      var data = await repository.fetchMenuDetail(id);
       menuDetail.value = data['menu'];
       toppings.assignAll(data['topping']);
       levels.assignAll(data['level']);
       totalPrice.value = (data['menu']['harga'] as num).toInt();
-    } else {
-      print('API Error: ${response.reasonPhrase}'); // Log API error
-      throw Exception('Failed to load menu detail');
+    } catch (e) {
+      print('Error fetching menu detail: $e');
     }
   }
 
--- lib/features/detail_menu/repositories/detail_menu_repository.dart
+++ lib/features/detail_menu/repositories/detail_menu_repository.dart
@@ -1,7 +1,33 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:venturo_core/features/detail_menu/constants/detail_menu_api_constant.dart';
 
 class DetailMenuRepository {
-  DetailMenuRepository._();
+  DetailMenuRepository();
 
   var apiConstant = DetailMenuApiConstant();
+  final String _token = dotenv.env['API_TOKEN'] ?? '';
+
+  Future<Map<String, dynamic>> fetchMenuDetail(int id) async {
+    var headers = {
+      'token': _token,
+    };
+    var request = http.Request(
+        'GET', Uri.parse('https://trainee.landa.id/javacode/menu/detail/$id'));
+
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      var responseData = await response.stream.bytesToString();
+      print('API Response Data: $responseData'); // Log API response
+      var data = json.decode(responseData)['data'];
+      return data;
+    } else {
+      print('API Error: ${response.reasonPhrase}'); // Log API error
+      throw Exception('Failed to load menu detail');
+    }
+  }
 }

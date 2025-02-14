090b5a4839be406717de86df648d735c8203ecee
Rama Pramudhita Bhaskara <2241720128@student.polinema.ac.id>
2025-02-14 14:24:23 +0700

    add : logger

--- lib/features/checkout/controllers/checkout_controller.dart
+++ lib/features/checkout/controllers/checkout_controller.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:logger/logger.dart';
 import 'package:venturo_core/features/detail_menu/repositories/detail_menu_repository.dart';
 
 class CheckoutController extends GetxController {
@@ -13,61 +14,94 @@ class CheckoutController extends GetxController {
       Rx<Map<String, dynamic>?>(null);
 
   final DetailMenuRepository detailMenuRepository = DetailMenuRepository();
+  final Logger logger = Logger();
 
   @override
   void onInit() {
     super.onInit();
     // Fetch menu list and calculate total price
-    fetchMenuDetails();
-    calculateTotal();
+    try {
+      logger.d('Initializing CheckoutController');
+      fetchMenuDetails();
+    } catch (e) {
+      logger.e('Error in CheckoutController onInit');
+    }
   }
 
   void fetchMenuDetails() async {
-    for (var menu in menuList) {
-      var menuDetail =
-          await detailMenuRepository.fetchMenuDetail(menu['id_menu']);
-      menu['foto'] = menuDetail['foto'];
+    try {
+      logger.d('Fetching menu details');
+      for (var menu in menuList) {
+        var menuDetail =
+            await detailMenuRepository.fetchMenuDetail(menu['id_menu']);
+        menu['foto'] = menuDetail['foto'];
+      }
+      menuList.refresh();
+      logger.d('Menu details fetched successfully');
+    } catch (e) {
+      logger.e('Error in fetchMenuDetails');
     }
-    menuList.refresh();
   }
 
   void calculateTotal() {
-    int total = 0;
-    int totalItems = 0;
-    for (var menu in menuList) {
-      total += (menu['harga'] as int) * (menu['jumlah'] as int);
-      totalItems += menu['jumlah'] as int;
+    try {
+      logger.d('Calculating total');
+      int total = 0;
+      int totalItems = 0;
+      for (var menu in menuList) {
+        total += (menu['harga'] as int) * (menu['jumlah'] as int);
+        totalItems += menu['jumlah'] as int;
+      }
+      totalHarga.value = total;
+      totalPembayaran.value =
+          (total - totalVoucherNominal.value).clamp(10000, total);
+      totalMenuDipesan.value = totalItems;
+      logger.d('Total calculated: $total, Total items: $totalItems');
+    } catch (e) {
+      logger.e('Error in calculateTotal');
     }
-    totalHarga.value = total;
-    totalPembayaran.value =
-        (total - totalVoucherNominal.value).clamp(10000, total);
-    totalMenuDipesan.value = totalItems;
   }
 
   void applyVoucher(Map<String, dynamic> voucher) {
-    selectedVoucher.value = voucher;
-    totalVoucherNominal.value = voucher['nominal'] as int;
-    calculateTotal();
+    try {
+      logger.d('Applying voucher: ${voucher['nominal']}');
+      selectedVoucher.value = voucher;
+      totalVoucherNominal.value = voucher['nominal'] as int;
+      calculateTotal();
+    } catch (e) {
+      logger.e('Error in applyVoucher');
+    }
   }
 
   Map<String, List<Map<String, dynamic>>> get groupedMenuByCategory {
-    Map<String, List<Map<String, dynamic>>> groupedMenu = {};
-    for (var menu in menuList) {
-      String category = menu['kategori'] ?? 'Lainnya';
-      if (!groupedMenu.containsKey(category)) {
-        groupedMenu[category] = [];
+    try {
+      logger.d('Grouping menu by category');
+      Map<String, List<Map<String, dynamic>>> groupedMenu = {};
+      for (var menu in menuList) {
+        String category = menu['kategori'] ?? 'Lainnya';
+        if (!groupedMenu.containsKey(category)) {
+          groupedMenu[category] = [];
+        }
+        groupedMenu[category]!.add(menu);
       }
-      groupedMenu[category]!.add(menu);
+      return groupedMenu;
+    } catch (e) {
+      logger.e('Error in groupedMenuByCategory');
+      return {};
     }
-    return groupedMenu;
   }
 
   void updateMenu(Map<String, dynamic> updatedMenu) {
-    int index = menuList
-        .indexWhere((menu) => menu['id_menu'] == updatedMenu['id_menu']);
-    if (index != -1) {
-      menuList[index] = updatedMenu;
-      calculateTotal();
+    try {
+      logger.d('Updating menu: ${updatedMenu['id_menu']}');
+      int index = menuList
+          .indexWhere((menu) => menu['id_menu'] == updatedMenu['id_menu']);
+      if (index != -1) {
+        menuList[index] = updatedMenu;
+        calculateTotal();
+      }
+    } catch (e) {
+      logger.e('Error in updateMenu');
     }
   }
 }

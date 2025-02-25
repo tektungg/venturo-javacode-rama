efdd382a1e56025b34f77136bd850ec23c0a564a
Rama Pramudhita Bhaskara <2241720128@student.polinema.ac.id>
2025-02-25 12:31:36 +0700

    add : left padding

--- lib/features/profile/view/components/section_title.dart
+++ lib/features/profile/view/components/section_title.dart
@@ -3,9 +3,12 @@ import 'package:get/get.dart';
 import 'package:venturo_core/shared/styles/color_style.dart';
 
 Widget buildSectionTitle(String title) {
-  return Text(
-    title.tr,
-    style: Get.textTheme.titleLarge!
-        .copyWith(color: ColorStyle.primary, fontWeight: FontWeight.bold),
+  return Padding(
+    padding:  const EdgeInsets.only(left: 16.0),
+    child: Text(
+      title.tr,
+      style: Get.textTheme.titleLarge!
+          .copyWith(color: ColorStyle.primary, fontWeight: FontWeight.bold),
+    ),
   );
 }
